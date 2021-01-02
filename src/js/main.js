9f6e058f79a93a238b071df0dc64fa7e7d21d9db
AlexCrow <iamalexcrow@gmail.com>
2021-01-01 18:27:19 -0600

    second project first commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+import modals from './modules/modals.js';
+import sliders from './modules/sliders.js';
+import forms from './modules/forms.js';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMorestyles';
+
+window.addEventListener('DOMContentLoaded', ()=> {
+    'use strict';
+
+    modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"');
+    checkTextInputs('[name="message"');
+    showMoreStyles('.button-styles','#styles .row');
+});
---
+++ src/js/modules/mask.js
@@ -0,0 +1,55 @@
+const mask = (selector) => {
+
+    let setCursorPosition = (pos, elem) => {
+        elem.addEventListener('click', () => {
+			elem.selectionStart = elem.selectionEnd = elem.value.length;
+		});
+        elem.focus();
+
+        if(elem.setSelectionRange) {
+            elem.setSelectionRange(pos, pos); 
+        } else if (elem.createTextRange) {
+            let range = elem.createTextRange();
+
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    };
+
+    function createMask(event) {
+        let matrix = '+7 (___) ___ __ __',
+            i = 0,
+            // не позволяет пользователю удалить цифры из матрицы 
+            def = matrix.replace(/\D/g, ''),
+            val = this.value.replace(/\D/g, '');
+                if (def.length >= val.length) {
+                    val = def;
+            }
+            this.value = matrix.replace(/./g,function (a) {
+                return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i>= val.length ? '' : a;
+            });
+
+            if (event.type === 'blur') {
+                if(this.value.length == 2) {
+                    this.value  = '';
+                }
+            } else {
+                setCursorPosition(this.value.length, this);
+            }
+            
+            if (event.type === 'click') {
+                event.selectionStart = event.selectionEnd = event.value.length;
+            }
+    }
+
+    let inputs = document.querySelectorAll(selector);
+
+    inputs.forEach(input => {
+        input.addEventListener('input',createMask);
+        input.addEventListener('focus',createMask);
+        input.addEventListener('blur',createMask);
+    });
+};
+export default mask;
