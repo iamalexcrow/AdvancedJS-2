8730e8b86173668081f247c255edede44f69778f
AlexCrow <iamalexcrow@gmail.com>
2021-01-01 21:09:33 -0600

    calculator with values

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import forms from './modules/forms.js';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMorestyles';
+import calc from './modules/calc';
 
 window.addEventListener('DOMContentLoaded', ()=> {
     'use strict';
@@ -16,4 +17,5 @@ window.addEventListener('DOMContentLoaded', ()=> {
     checkTextInputs('[name="name"');
     checkTextInputs('[name="message"');
     showMoreStyles('.button-styles','#styles .row');
+    calc('#size','#material', '#options', '.promocode', '.calc-price');
 });
