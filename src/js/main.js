8decebb75c0636a32e1fb18764fc9471bdd7b3b3
AlexCrow <iamalexcrow@gmail.com>
2021-01-04 08:51:46 -0600

    final version

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,8 @@ import pictureSize from './modules/pictureSize';
 import accordion from './modules/accordion';
 import burger from './modules/burger';
 import scrolling from './modules/scrolling';
+import drop from './modules/drop';
+
 
 window.addEventListener('DOMContentLoaded', ()=> {
     'use strict';
@@ -28,4 +30,5 @@ window.addEventListener('DOMContentLoaded', ()=> {
     accordion('.accordion-heading');
     burger('.burger-menu', '.burger');
     scrolling('.pageup'); 
+    drop();
 });
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -38,7 +38,7 @@ const forms = () => {
             let dots;
             const arr = item.files[0].name.split('.');
             arr[0].length > 5 ? dots='...' : dots = '.';
-            const name = arr[0].substring(0, 5) + dots + arr[1];
+            const name = arr[0].substring(0, 6) + dots + arr[1];
             item.previousElementSibling.textContent = name;
         });
     });
